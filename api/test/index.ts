f12476b94b31aba25ff8b7a324433893b6ec4fda
Andrew Burgess <andrew.burgess@classdojo.com>
2023-07-26 10:59:58 -0400

    vercel test route

---
+++ api/test/index.ts
@@ -0,0 +1,6 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+module.exports = function handler(req: VercelRequest, res: VercelResponse) {
+  console.log(req.method, req.url);
+  res.status(200).json({ hello: "world" });
+};
