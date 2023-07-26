4e089558721a18bd2aefe6ec3894dadae6eb0a9a
Andrew Burgess <andrew.burgess@classdojo.com>
2023-07-26 17:35:07 -0400

    add Drafts script

---
+++ scripts/drafts-step1-prompt-for-tokens.js
@@ -0,0 +1,32 @@
+const credential = Credential.create("Pidge", "Insert pidge secret");
+credential.addPasswordField("pidgetoken", "Pidge Token");
+credential.addPasswordField("githubtoken", "Github Token");
+
+credential.authorize();
+
+const pidgeToken = credential.getValue("pidgetoken").toString().trim();
+const githubToken = credential.getValue("githubtoken").toString().trim();
+const pidgeEndpoint = "https://pidge.vercel.app/api/note";
+
+const obj = {
+  url: pidgeEndpoint,
+  method: "POST",
+  data: {
+    pidgeToken,
+    githubToken,
+    note: {
+      text: draft.content,
+    },
+  },
+};
+
+console.log("req: " + JSON.stringify(obj, null, "  "));
+const req = HTTP.create();
+const response = req.request(obj);
+
+if (response.statusCode != 200 && response.statusCode != 202) {
+  console.log("Request failed. Status code: " + response.statusCode);
+  context.fail();
+} else {
+  console.log("res: " + JSON.stringify(response.responseData, null, "  "));
+}
